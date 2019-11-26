b775a4438edcab7cf45bdfec0c7cd791bdd0edbb
Qurrota <qurrotaayunin36@gmail.com>
2019-11-26 18:39:36 +0700

    mengubah nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Qurrota A'yunin
                 </div>
 
                 <div class="links">
