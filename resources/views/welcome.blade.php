8d6199a1269ea0a598feae6c5898c1f4750d77af
Qurrota <qurrotaayunin36@gmail.com>
2019-12-06 14:52:49 +0700

    konflik ayun

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Qurrota A'yunin
+                    Konflik branch Ayun
                 </div>
 
                 <div class="links">
